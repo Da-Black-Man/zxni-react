d84695fe5814966e2646ef0c7eb4805cff7aa301
Seni <dablackoo@gmail.com>
2020-03-27 08:06:51 -0700

    update

---
+++ src/components/loader/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="o-loader">
+      <div className="o-loader__line"></div>
+    </div>
+  );
+};
+
+export default Loader;
